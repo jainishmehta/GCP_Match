514f21ae66ca81819d65526f7ee230ffc10c7aab
Jainish Mehta <jainishmehta09@gmail.com>
2025-01-05 23:40:22 -0700

    Updated so top 5 labels outputted when image uploaded

--- frontend_express_app/convert_upload.sh
+++ frontend_express_app/convert_upload.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-
-image_name=${req.file.filename}
-echo $image_name
-base64_image_name='base64_'${image}
-base64 ${image_name} -w 0 > ${base64_image_name}
+PROJECT_ID="812751601631"
+REQUEST_JSON="request.json"
+OUTPUT_FILE="annotations.txt"
+> "$OUTPUT_FILE" 
+for image in *.jpg;
+    do
+    python3 base64_converter.py ${image}
+    response=$(curl -X POST \
+        -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+        -H "x-goog-user-project: $PROJECT_ID" \
+        -H "Content-Type: application/json; charset=utf-8" \
+        -d @$REQUEST_JSON \
+        "https://vision.googleapis.com/v1/images:annotate")
+    jq -r '.requests[].image.content' "$REQUEST_JSON" >> "$OUTPUT_FILE"
+    echo "$response" | jq -r '.responses[].labelAnnotations[] | {mid, description, score, topicality}' >> "$OUTPUT_FILE"
+    echo "-----" >> "$OUTPUT_FILE"
+done
